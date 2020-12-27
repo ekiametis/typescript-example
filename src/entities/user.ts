4d60b573e40bc4552b2267082367102d67eb803f
Emmanuel <emmanuelkiametis@localhost.localdomain>
2020-12-27 20:02:46 -0300

    Adding unit tests

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,4 +1,4 @@
-import { uuid } from 'uuidv4';
+import { v4 as uuid } from 'uuid';
 import { IEntity } from './entity';
 
 export class User implements IEntity{
---
+++ src/use-cases/create-user/create-user-use-case.spec.ts
@@ -0,0 +1,25 @@
+import { expect } from 'chai';
+import { createUserUseCase } from '.';
+import { UserAlreadyExist } from '../../exceptions/users/user-already-exists';
+
+describe('Create User Tests', () => {
+
+    it('should create a user with success', async () => {
+        const payload = { name: 'Emmanuel Kiametis', email: 'kiametis91@gmail.com', birthday: '1991-08-28' }
+        const user = await createUserUseCase.execute(payload);
+        expect(user).have.property('id');
+        expect(user.name).to.be.eq(payload.name);
+        expect(user.email).to.be.eq(payload.email);
+        expect(user.birthday).to.be.eq(payload.birthday);
+    });
+
+    it('should not create a user because he already exists', async () => {
+        try {
+            const payload = { name: 'Emmanuel Kiametis', email: 'kiametis91@gmail.com', birthday: '1991-08-28' }
+            await createUserUseCase.execute(payload);
+            await createUserUseCase.execute(payload);
+        } catch(err) {
+            expect(err).to.be.an.instanceof(UserAlreadyExist);
+        }
+    });
+})
