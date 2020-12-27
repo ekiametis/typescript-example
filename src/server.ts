558a2f066ddb7993e0e19faf9cbfc7a0e326666d
Emmanuel <emmanuelkiametis@localhost.localdomain>
2020-12-27 20:12:33 -0300

    Adjusting express listener

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import { app } from "./app";
 
 const PORT = process.env.PORT || 3333;
 
-app.listen(() => {
+app.listen(PORT, () => {
     console.log(`Application is listening on port ${PORT}`)
 });
