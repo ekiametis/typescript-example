d8fc378c1338d258e60f4ea80fe3fc6d50c3922c
Emmanuel <emmanuelkiametis@localhost.localdomain>
2020-12-31 16:36:07 -0300

    Verifying openapi error type

--- src/middlewares/error-handling-middleware.ts
+++ src/middlewares/error-handling-middleware.ts
@@ -1,6 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import { HttpStatusCode } from "../enums/http-status-code";
 import { IHttpException, HttpException } from "../exceptions/http/http-exception";
+import { HttpError } from 'express-openapi-validator/dist/framework/types';
+
+const isOpenApiException = (err: Error): Boolean => {
+    return ( err instanceof HttpError );
+}
 
 const errorHandlingMiddleware = (err: Error, req: Request, res: Response, next: NextFunction): void => {
     let httpStatusCode: HttpStatusCode = HttpStatusCode.INTERNAL_SERVER_ERROR;
@@ -8,6 +13,12 @@ const errorHandlingMiddleware = (err: Error, req: Request, res: Response, next: 
     if(err instanceof HttpException) {
         httpStatusCode = err.retrieveHttpStatusCode();
         payload = err.formatToJSON();
+    } else if(isOpenApiException(err)) {
+        const castError = <HttpError> err;
+        const formattedError = HttpException.build(castError.status, 0, castError.message);
+        httpStatusCode = formattedError.retrieveHttpStatusCode();
+        payload = formattedError.formatToJSON();
+
     }
     res.status(httpStatusCode).json(payload);
     next();
