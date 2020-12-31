c86141cf883601eba2dea6ae4257874876772717
Emmanuel <emmanuelkiametis@localhost.localdomain>
2020-12-31 16:56:16 -0300

    Versioning openapi specification

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,11 @@ import { errorHandlingMiddleware } from './middlewares/error-handling-middleware
 import { requestContextMiddleware } from './middlewares/request-context-middleware';
 import { openApiValidatorMiddleware } from './middlewares/open-api-middleware';
 
+const CURRENT_VERSION = process.env.CURRENT_VERSION;
+
 const app = express();
 app.use(express.json());
-app.use(openApiValidatorMiddleware(path.join(__dirname, 'openapi.yaml')))
+app.use(openApiValidatorMiddleware(path.join(__dirname, `openapi/openapi-${CURRENT_VERSION}.yaml`)))
 app.use(requestContextMiddleware);
 app.use(router);
 app.use(errorHandlingMiddleware);
